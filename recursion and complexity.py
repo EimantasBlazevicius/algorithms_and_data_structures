30910102da7b7d43b1a1ce56aa2542ffb3308312
Eimantas Blaževičius <44463166+EimantasBlazevicius@users.noreply.github.com>
2023-07-30 08:27:13 +0300

    Update recursion and complexity.py

--- recursion and complexity.py
+++ recursion and complexity.py
@@ -92,3 +92,48 @@ def fibonacci_iter(n):
 
 for n in range(1, 99):
     print(n, ": ", fibonacci_iter(n))
+
+
+
+def binary_search_rec(arr, low, high, x):
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search_rec(arr, low, mid - 1, x)
+        else:
+            return binary_search_rec(arr, mid + 1, high, x)
+    else:
+        return -1
+
+
+def binary_search_iter(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+arr = [2, 3, 4, 10, 40]
+x = 10
+
+result = binary_search_rec(arr, 0, len(arr) - 1, x)
+result2 = binary_search_iter(arr, x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
